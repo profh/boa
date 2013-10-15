0fac3884c8beffaef7e2f1c379eb7fd3e8a1224c
Jonathan Chung <juc@andrew.cmu.edu>
2013-10-15 18:08:12 -0400

    Filled in ToDo related model details

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,3 +1,15 @@
 class ToDo < ActiveRecord::Base
+	attr_accessible :title, :notes, :created_by, :completed_by, :due_date, :assigned_person, :todo_category
+
+	# Relationships
+	has_many :todo_categories
+
+	# Validations
+	validates :title, :notes, :created_by, :completed_by, :due_date, :assigned_person, :presence => true
+	validates :due_date, :date => { :after => :created_at }
+
+	# Scopes
+	default_scope order('due_date DESC')
+	
   
 end
--- app/models/to_do_category.rb
+++ app/models/to_do_category.rb
@@ -1,3 +1,11 @@
 class ToDoCategory < ActiveRecord::Base
+	attr_accessible :name
+
+	# Relationships
+
+	# Validations
+	validates :name, :presence => true
+
+	# Scopes
   
 end
